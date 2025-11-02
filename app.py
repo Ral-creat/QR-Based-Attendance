706b994f87906e43d3aa868e5e49eb500c492293
Ral-creat <laramaemvidal@asscat.edu.ph>
2025-11-02 18:35:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import plotly.express as px
 # --- PAGE CONFIG ---
 st.set_page_config(page_title="✨ Smart Attendance System", layout="wide", page_icon="🗓️")
 
+# --- LOAD EXTERNAL CSS ---
+with open("style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 # --- SIDEBAR: UPLOAD DATASET ---
 st.sidebar.header("📂 Upload Attendance File")
 uploaded_file = st.sidebar.file_uploader("CSV or Excel", type=["csv", "xlsx"])
